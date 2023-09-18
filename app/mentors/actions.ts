ef3a2cba37bd621b35e996821b3da2cec3b1093b
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-09-18 12:53:34 +0300

    removed some things from actions.ts

--- app/mentors/actions.ts
+++ app/mentors/actions.ts
@@ -73,19 +73,6 @@ export const updateMentor = zact(formData)(async (formData) => {
   return res;
 });
 
-export const getMentor = async (email: string) => {
-  const mentor = await db
-    .select()
-    .from(mentors)
-    .where(eq(mentors.email, email));
-  return mentor[0];
-};
-
-export const getAllMentors = async () => {
-  const allMentors = await db.select().from(mentors);
-  return allMentors;
-};
-
 // export const checkifFileExists = async (fileName: string) => {
 //   const file = await db
 //     .select()
