25914a8071bdd1d2a15f86caccf37f0ce610cc4a
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-09-20 15:44:30 +0300

    fixed security vulnerability

--- app/teams/actions.ts
+++ app/teams/actions.ts
@@ -4,16 +4,23 @@ import { eq } from "drizzle-orm";
 
 import { particpants, teams } from "~/app/db/schema";
 import { db } from "../db";
+import { getParticipantFromSession } from "../participants/service";
 
-export async function deleteTeamById(teamId: string) {
-  //delete team by id
+export async function deleteMyTeam() {
+  const participant = await getParticipantFromSession();
+
   try {
-    await db
-      .update(particpants)
-      .set({ teamId: null, isCaptain: false })
-      .where(eq(particpants.teamId, teamId));
-    await db.delete(teams).where(eq(teams.id, teamId)).returning();
-    return { success: true };
+    if (participant?.team.id) {
+      await db
+        .update(particpants)
+        .set({ teamId: null, isCaptain: false })
+        .where(eq(particpants.teamId, participant.team.id));
+      await db
+        .delete(teams)
+        .where(eq(teams.id, participant?.team.id))
+        .returning();
+      return { success: true };
+    }
   } catch (e) {
     console.log(e);
     return { success: false, message: e };
