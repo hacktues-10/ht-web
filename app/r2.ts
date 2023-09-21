9df60dc4862647da66f97fb84e50de183d5b5b06
bvpav <bvpav@tutanota.com>
2023-09-21 23:16:06 +0300

    refactor: removed weird `as`

--- app/r2.ts
+++ app/r2.ts
@@ -15,8 +15,8 @@ const S3 = new S3Client({
   region: "auto",
   endpoint: `https://${env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
-    accessKeyId: env.S3_UPLOAD_KEY as string,
-    secretAccessKey: env.S3_UPLOAD_SECRET as string,
+    accessKeyId: env.S3_UPLOAD_KEY,
+    secretAccessKey: env.S3_UPLOAD_SECRET,
   },
 });
 
