1fbe679ebd398ec78ec11734ce920c16e2e3f63e
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-10-24 22:40:20 +0300

    working create team role and channels

--- app/api/discord/actions.ts
+++ app/api/discord/actions.ts
@@ -39,3 +39,142 @@ export const removeDiscordRole = async (userId: string, roleId: string) => {
 
   return { success: true };
 };
+
+export const CreateDiscordTeam = async (teamId: string) => {
+  try {
+    // Create the role with specified permissions
+    const roleData = await createRole(teamId);
+    console.log(`Role created successfully ${roleData}`);
+    // Create the section and channels with permission overwrites
+    await createSectionAndChannels(teamId, roleData.id);
+
+    console.log(`Role created: ${roleData.name}`);
+    return roleData.id;
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
+};
+
+const createRole = async (teamId: string) => {
+  const headers = {
+    Authorization: "Bot " + env.DISCORD_BOT_ID,
+    "Content-Type": "application/json",
+  };
+
+  const params = {
+    name: teamId,
+    permissions: 1048576,
+    mentionable: true,
+  };
+
+  const roleResponse = await fetch(
+    "https://discord.com/api/guilds/" + env.DISCORD_GUILD_ID + "/roles",
+    {
+      headers: headers,
+      method: "POST",
+      body: JSON.stringify(params),
+    },
+  );
+
+  if (roleResponse.ok) {
+    return await roleResponse.json();
+  }
+  return false;
+};
+
+const createSectionAndChannels = async (teamId: string, roleId: string) => {
+  const section = {
+    name: "Team " + teamId,
+    textChannels: [teamId],
+    voiceChannels: [teamId],
+  };
+
+  const sectionResponse = await fetch(
+    `https://discord.com/api/v10/guilds/${env.DISCORD_GUILD_ID}/channels`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bot ${env.DISCORD_BOT_ID}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: section.name,
+        type: 4,
+        permission_overwrites: [
+          {
+            id: env.DISCORD_GUILD_ID, // @everyone
+            type: 0,
+            deny: 1024, // VIEW_CHANNEL (allow viewing for the role)
+          },
+          {
+            id: roleId,
+            type: 0,
+            allow: 1024,
+          },
+        ],
+      }),
+    },
+  );
+
+  if (sectionResponse.ok) {
+    const sectionData = await sectionResponse.json();
+
+    console.log(sectionData.id);
+
+    // Create text channels in the section
+    for (const textChannelName of section.textChannels) {
+      await createChannel(sectionData.id, textChannelName, 0, roleId);
+    }
+
+    // Create voice channels in the section
+    for (const voiceChannelName of section.voiceChannels) {
+      await createChannel(sectionData.id, voiceChannelName, 2, roleId);
+    }
+  } else {
+    console.error("Error creating section:", sectionResponse.statusText);
+  }
+};
+
+const createChannel = async (
+  parentId: string,
+  channelName: string,
+  channelType: number,
+  roleId: string,
+) => {
+  const params = JSON.stringify({
+    name: channelName,
+    type: channelType,
+    parent_id: parentId,
+    permission_overwrites: [
+      {
+        id: env.DISCORD_GUILD_ID,
+        type: 0,
+        deny: 1024,
+      },
+      {
+        id: roleId,
+        type: 0,
+        allow: 1024,
+      },
+    ],
+  });
+
+  const channelResponse = await fetch(
+    `https://discord.com/api/v10/guilds/${env.DISCORD_GUILD_ID}/channels`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bot ${env.DISCORD_BOT_ID}`,
+        "Content-Type": "application/json",
+      },
+      body: params,
+    },
+  );
+
+  if (!channelResponse.ok) {
+    console.error(
+      `Error creating channel (${channelType}):`,
+      channelResponse.statusText,
+    );
+  }
+};
--- app/teams/service.ts
+++ app/teams/service.ts
@@ -2,8 +2,9 @@ import { eq } from "drizzle-orm";
 import invariant from "tiny-invariant";
 import { slugify } from "transliteration";
 
+import { addDiscordRole, CreateDiscordTeam } from "~/app/api/discord/actions";
 import { db } from "../db";
-import { particpants, teams } from "../db/schema";
+import { discord_table, particpants, teams } from "../db/schema";
 
 export async function getConfirmedTeams() {
   return db.select().from(teams);
@@ -19,16 +20,26 @@ export async function createTeam(team: {
   description: string;
   captainId: number;
 }) {
+  const roleId = await CreateDiscordTeam(slugify(team.name));
+
   // TODO: verify if name is ok
   const results = await db
     .insert(teams)
     .values({
       id: slugify(team.name),
+      roleId: roleId,
       ...team,
     })
     .returning({ id: teams.id });
   const insertedTeam = results.at(0);
   invariant(insertedTeam, "Failed to create team");
+  const discordMember = await db
+    .select()
+    .from(discord_table)
+    .where(eq(discord_table.participant_id, team.captainId));
+  if (discordMember.length < 1 || !discordMember[0].discord_id)
+    return { success: false };
+  await addDiscordRole(discordMember[0].discord_id, roleId);
   await db
     .update(particpants)
     .set({
