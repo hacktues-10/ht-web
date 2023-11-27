89d0b6d9cda0fb3bfd8fd43462a18e2f6c47c333
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-11-27 19:05:29 +0200

    before avatar installation

--- app/components/Team/renderMember.tsx
+++ app/components/Team/renderMember.tsx
@@ -1,4 +1,9 @@
 import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "~/app/components/ui/avatar";
+import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
@@ -24,8 +29,71 @@ export default function RenderMember({
         </div>
       </HoverCardTrigger>
       <HoverCardContent>
-        The React Framework – created and maintained by @vercel.
+        <div className="flex justify-between space-x-4">
+          <Avatar>
+            <AvatarImage />
+            <AvatarFallback>
+              {member.firstName?.charAt(0).toUpperCase()}
+            </AvatarFallback>
+          </Avatar>
+          <div className="space-y-1">
+            <h4 className="text-sm font-semibold">@nextjs</h4>
+            <p className="text-sm">
+              The React Framework – created and maintained by @vercel.
+            </p>
+            <div className="flex items-center pt-2">
+              {/* <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "} */}
+              <span className="text-xs text-muted-foreground">
+                Joined December 2021
+              </span>
+            </div>
+          </div>
+        </div>
       </HoverCardContent>
     </HoverCard>
   );
 }
+
+// import { CalendarDays } from "lucide-react"
+
+// import {
+//   Avatar,
+//   AvatarFallback,
+//   AvatarImage,
+// } from "@/components/ui/avatar"
+// import { Button } from "@/components/ui/button"
+// import {
+//   HoverCard,
+//   HoverCardContent,
+//   HoverCardTrigger,
+// } from "@/components/ui/hover-card"
+
+// export function HoverCardDemo() {
+//   return (
+//     <HoverCard>
+//       <HoverCardTrigger asChild>
+//         <Button variant="link">@nextjs</Button>
+//       </HoverCardTrigger>
+//       <HoverCardContent className="w-80">
+//         <div className="flex justify-between space-x-4">
+//           <Avatar>
+//             <AvatarImage src="https://github.com/vercel.png" />
+//             <AvatarFallback>VC</AvatarFallback>
+//           </Avatar>
+//           <div className="space-y-1">
+//             <h4 className="text-sm font-semibold">@nextjs</h4>
+//             <p className="text-sm">
+//               The React Framework – created and maintained by @vercel.
+//             </p>
+//             <div className="flex items-center pt-2">
+//               <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+//               <span className="text-xs text-muted-foreground">
+//                 Joined December 2021
+//               </span>
+//             </div>
+//           </div>
+//         </div>
+//       </HoverCardContent>
+//     </HoverCard>
+//   )
+// }
