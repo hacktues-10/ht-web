77eeedd705e0497993602c5e4c428bc5b87717e0
bvpav <bvpav@tutanota.com>
2023-12-04 21:37:32 +0200

    Began Hourglass

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
 import { getHTSession } from "./api/auth/session";
 import { SignInButton, SignOutButton } from "./components/buttons";
+import { Hourglass, Test } from "./components/hourglass";
 
 export default async function Home() {
-  const session = await getHTSession();
-  if (session) {
-    return (
-      <>
-        Signed in as {session?.user?.email} <br />
-        <SignOutButton>Sign out</SignOutButton>
-      </>
-    );
-  } else {
-    return (
-      <div>
-        <SignInButton>Login</SignInButton>
-      </div>
-    );
-  }
+  return <Test />;
+  // return <Hourglass fillAmount={0.5} />;
+  // const session = await getHTSession();
+  // if (session) {
+  //   return (
+  //     <>
+  //       Signed in as {session?.user?.email} <br />
+  //       <SignOutButton>Sign out</SignOutButton>
+  //     </>
+  //   );
+  // } else {
+  //   return (
+  //     <div>
+  //       <SignInButton>Login</SignInButton>
+  //     </div>
+  //   );
+  // }
 }
