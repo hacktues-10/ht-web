b4bb7dbadacd4b6430321fc05ff6033a4bed3313
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-12-08 23:41:17 +0200

    fixed the email :)

--- app/api/auth/options.ts
+++ app/api/auth/options.ts
@@ -74,49 +74,57 @@ export const authOptions = {
 function html(params: { url: string; host: string; theme: any }) {
   const { url, host, theme } = params;
 
-  const escapedHost = host.replace(/\./g, "&#8203;.");
-
-  const brandColor = theme.brandColor || "#346df1";
   const color = {
     background: "#f9f9f9",
-    text: "#444",
-    mainBackground: "#fff",
-    buttonBackground: brandColor,
-    buttonBorder: brandColor,
+    text: "#fff",
+    mainBackground: "#0F172A",
+    buttonBorder: "#fff",
     buttonText: theme.buttonText || "#fff",
   };
 
+  const buttonStyle = `
+    color: ${color.buttonText};
+    text-decoration: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    display: inline-block;
+    font-weight: bold;
+    background: #b01c32;
+  `;
+  // border: 1px solid ${color.buttonBorder};
+
   return `
-<body style="background: ${color.background};">
-  <table width="100%" border="0" cellspacing="20" cellpadding="0"
-    style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
-    <tr>
-      <td align="center"
-        style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        Sign in to <strong>${escapedHost}</strong>
-      </td>
-    </tr>
-    <tr>
-      <td align="center" style="padding: 20px 0;">
-        <table border="0" cellspacing="0" cellpadding="0">
-          <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${color.buttonBackground}"><a href="${url}"
-                target="_blank"
-                style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${color.buttonText}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${color.buttonBorder}; display: inline-block; font-weight: bold;">Sign
-                in</a></td>
-          </tr>
-        </table>
-      </td>
-    </tr>
-    <tr>
-      <td align="center"
-        style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
-        If you did not request this email you can safely ignore it.
-      </td>
-    </tr>
-  </table>
-</body>
-`;
+    <body style="background: ${color.background};">
+      <table width="100%" border="0" cellspacing="20" cellpadding="0"
+        style="background: ${color.mainBackground}; max-width: 600px; margin: auto; border-radius: 10px;">
+        <tr>
+          <td align="center"
+            style="padding: 10px 0px; font-size: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+            Влезте в <strong>Hack TUES X</strong>
+          </td>
+        </tr>
+        <tr>
+          <td align="center" style="padding: 20px 0;">
+            <table border="0" cellspacing="0" cellpadding="0">
+              <tr>
+                <td align="center" style="border-radius: 5px;">
+                  <a href="${url}" target="_blank" style="${buttonStyle}">
+                    Влез
+                  </a>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr>
+          <td align="center"
+            style="padding: 0px 0px 10px 0px; font-size: 16px; line-height: 22px; font-family: Helvetica, Arial, sans-serif; color: ${color.text};">
+            Ако не сте поискали този имейл, можете спокойно да го игнорирате.
+          </td>
+        </tr>
+      </table>
+    </body>
+  `;
 }
 
 function text({ url, host }: { url: string; host: string }) {
