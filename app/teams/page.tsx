96ab1483e733106bf18aba83b384972cb6e0e8f1
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-12-08 21:10:13 +0200

    confirmed teams number display

--- app/teams/page.tsx
+++ app/teams/page.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Link from "next/link";
 
 import {
@@ -8,6 +7,7 @@ import {
   TabsTrigger,
 } from "~/app/components/ui/tabs";
 import { IfHTFeatureOn } from "../_integrations/components";
+import { getConfirmedTeamsNumber } from "../(full-layout)/teams/actions";
 import { getConfirmedTeams } from "../(full-layout)/teams/service";
 import TeamCard from "../components/Team/teamCard";
 import { Button } from "../components/ui/button";
@@ -26,6 +26,9 @@ export default async function TeamList() {
       studentTeams.push(te);
     }
   });
+
+  const confirmedStudentTeamsNumber = await getConfirmedTeamsNumber(false);
+  const confirmedAlumniTeamsNumber = await getConfirmedTeamsNumber(true);
 
   //   TODO: move elsewhere
   return (
@@ -51,21 +54,18 @@ export default async function TeamList() {
             <h1 className="mt-8 self-center font-mono text-3xl font-semibold italic text-white sm:text-4xl">
               Отбори на ученици
             </h1>
-            {studentTeams.length < 1 && (
+            {studentTeams.length > 1 ? (
+              <h2 className="mt-8 self-center font-mono text-2xl font-semibold italic text-white sm:text-3xl">
+                Потвърдени отбори: {confirmedStudentTeamsNumber}/70
+              </h2>
+            ) : (
               <h2 className="mt-8 self-center font-mono text-2xl font-semibold italic text-white sm:text-3xl">
                 Все още няма потвърдени отбори.
               </h2>
             )}
             <div className="inline-grid w-full grid-cols-1 gap-5 py-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {studentTeams.map((team) => (
-                <>
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                </>
+                <TeamCard team={team} key={team.id} />
               ))}
             </div>
           </div>
@@ -75,21 +75,19 @@ export default async function TeamList() {
             <h1 className="mt-8 self-center font-mono text-3xl font-semibold italic text-white sm:text-4xl">
               Отбори на завършили
             </h1>
-            {graduateTeams.length < 1 && (
+            {graduateTeams.length > 1 ? (
+              <h2 className="mt-8 self-center font-mono text-2xl font-semibold italic text-white sm:text-3xl">
+                Потвърдени отбори: {confirmedAlumniTeamsNumber}/20
+              </h2>
+            ) : (
               <h2 className="mt-8 self-center font-mono text-2xl font-semibold italic text-white sm:text-3xl">
                 Все още няма потвърдени отбори.
               </h2>
             )}
+
             <div className="inline-grid w-full grid-cols-1 gap-5 py-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
               {graduateTeams.map((team) => (
-                <>
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                  <TeamCard team={team} />
-                </>
+                <TeamCard team={team} key={team.id} />
               ))}
             </div>
           </div>
