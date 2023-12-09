0aaa8ca92daf7203d27e55e54dc47f6c5e7bcb11
bvpav <78971592+bvpav@users.noreply.github.com>
2023-12-09 22:42:57 +0200

    more tweals

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ import {
 
 export default async function Home() {
   return (
-    <div className="flex min-h-fit w-full flex-col gap-4">
+    <div className="flex min-h-fit w-full flex-col">
       <CountdownHero />
       <section className="light relative flex flex-col gap-14 overflow-x-visible bg-sand pb-14 pt-28 text-sand-foreground">
         <div className="absolute -left-[calc(100vw-100%)] bottom-0 top-0 -z-10 h-full w-[calc(100vw+(100vw-100%)/2)] bg-sand" />
@@ -38,8 +38,13 @@ export default async function Home() {
           <PodkrepqTitle>Партньори</PodkrepqTitle>
           <PodkrepqPackageScrollableContent podkrepqshti={PARTNERS} />
         </PordkrepqPackage>
+        <div className="py-9" />
       </section>
-      <section className="flex flex-col items-center gap-3 pt-7">
+      <section className="relative flex flex-col items-center gap-3 pt-7">
+        <div
+          className="absolute -top-3 left-1/2 -z-30 h-[34px] min-h-[800px] w-[300%] -translate-x-1/2 bg-[radial-gradient(ellipse_50%_50%_at_50%_0%,hsl(var(--sand)/0.15),transparent)]"
+          aria-hidden
+        />
         <h2 className="scroll-m-20 pt-7 text-center text-5xl font-extrabold tracking-tight first:mt-0">
           Медиите за нас
         </h2>
@@ -48,6 +53,7 @@ export default async function Home() {
           <MediaArticleCard key={article.title} article={article} />
         ))}
       </section>
+      <div className="pb-4"></div>
     </div>
   );
 }
@@ -62,7 +68,7 @@ function CountdownHero() {
             aria-hidden
           />
           Hack TUES&nbsp;
-          <span className="bg-[radial-gradient(farthest-corner_at_100%_32%,#ffdea6ff_3%,transparent_45%)] bg-clip-text text-transparent">
+          <span className="bg-[radial-gradient(farthest-corner_at_100%_50%,#ffdea6a3_3%,transparent_45%)] bg-clip-text text-transparent">
             X
           </span>
         </h1>
@@ -122,23 +128,23 @@ function PodkrepqTitle({ children }: PropsWithChildren<{}>) {
   );
 }
 
-function MediaArticleCard({ article }: { article: MediaArticle }) {
-  const dateFormatter = new Intl.DateTimeFormat("bg", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+const dateFormatter = new Intl.DateTimeFormat("bg", {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
 
+function MediaArticleCard({ article }: { article: MediaArticle }) {
   return (
     <Link href={article.url} className="block" target="_blank">
       <Card className="flex max-w-2xl flex-col transition-transform hover:scale-105 sm:flex-row">
-        <Card className="relative grid aspect-video flex-1 shrink-0 place-content-center overflow-clip p-4 sm:w-1/2">
+        <figure className="relative grid aspect-video flex-1 shrink-0 place-content-center overflow-clip border-b bg-card/20 p-4 text-card-foreground shadow-sm backdrop-blur-md sm:w-1/2 sm:border-b-0 sm:border-r">
           <Image
             className="max-h-full max-w-full object-contain px-3 py-5 transition-transform"
             src={article.logo}
             alt={article.name}
           />
-        </Card>
+        </figure>
         <div className="flex-1">
           <CardHeader className="pb-3 sm:pb-6">
             <CardTitle className="text-center sm:text-left">
