81bb41d176e5310a06e09d548b801422e591383a
bvpav <bvpav@tutanota.com>
2023-12-11 15:02:36 +0200

    Delete notification loader component and page

--- app/notifications/page.tsx
+++
@@ -1,9 +0,0 @@
-import NotificationLoader from "~/app/notifications/_components/notification-loader";
-
-export default async function Home() {
-  return (
-    <div>
-      <NotificationLoader />
-    </div>
-  );
-}
