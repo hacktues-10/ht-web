2371ff0b3c7687d7e23c3cb170a2d5ad303ce234
bvpav <78971592+bvpav@users.noreply.github.com>
2023-12-13 18:45:16 +0200

    made it pass all cookies and simplified logic

--- middleware.ts
+++ middleware.ts
@@ -2,35 +2,27 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { env } from "./app/env.mjs";
 
-const hasCallbackUrl = (pathname: string) => {
-  return pathname === "/discord" || pathname === "/user/configure";
-};
-
 export async function middleware(request: NextRequest) {
   if (request.cookies.has("next-auth.session-token")) {
     const cookieHeader = request.headers.get("cookie");
     const credentials = cookieHeader ? "include" : "same-origin";
     const response = await fetch(`${env.NEXTAUTH_URL}api/checkAuthentication`, {
       credentials,
-      headers: {
-        cookie: `next-auth.session-token=${request.cookies.get(
-          "next-auth.session-token",
-        )?.value}`,
-      },
+      headers: cookieHeader ? { cookie: cookieHeader } : {},
     });
     const { isMentorOrParticipant, hasConnectedDiscord } = (
       await response.json()
     ).body;
 
-    if (!hasCallbackUrl(request.nextUrl.pathname)) {
-      if (!isMentorOrParticipant) {
+    if (!isMentorOrParticipant) {
+      if (request.nextUrl.pathname !== "/user/configure") {
         return NextResponse.redirect(
           `${env.NEXTAUTH_URL}user/configure?callbackUrl=` +
             encodeURIComponent(request.nextUrl.href),
         );
       }
-
-      if (!hasConnectedDiscord) {
+    } else if (!hasConnectedDiscord) {
+      if (request.nextUrl.pathname !== "/discord") {
         return NextResponse.redirect(
           `${env.NEXTAUTH_URL}discord?callbackUrl=` +
             encodeURIComponent(request.nextUrl.href),
