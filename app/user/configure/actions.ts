19bf7da3546d41c9d0760e3283e90a06db5cdaa1
bvpav <78971592+bvpav@users.noreply.github.com>
2023-12-13 11:46:40 +0200

    handled duplicate phone numbers better

--- app/user/configure/actions.ts
+++ app/user/configure/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { eq } from "drizzle-orm";
+import { DrizzleError, eq } from "drizzle-orm";
 import { zact } from "zact/server";
 import { z } from "zod";
 
@@ -12,59 +12,79 @@ import { db } from "../../db/index";
 import { alunmiRegistrationSchema } from "./schemas";
 
 export const registerAlumni = zact(alunmiRegistrationSchema)(async (data) => {
-  const gb = await getServerSideGrowthBook();
-  console.log({
-    isOff: gb.isOff("register-alumni"),
-    isOn: gb.isOn("register-alumni"),
-    feature1: gb.getFeatureValue("register-alumni", false),
-    feature2: gb.getFeatureValue("register-alumni", true),
-    features: gb.getFeatures(),
-  });
-  if (gb.isOff("register-alumni")) {
+  try {
+    const gb = await getServerSideGrowthBook();
+    console.log({
+      isOff: gb.isOff("register-alumni"),
+      isOn: gb.isOn("register-alumni"),
+      feature1: gb.getFeatureValue("register-alumni", false),
+      feature2: gb.getFeatureValue("register-alumni", true),
+      features: gb.getFeatures(),
+    });
+    if (gb.isOff("register-alumni")) {
+      return {
+        success: false,
+        error: "Регистрацията на завършили не е позволена по това време.",
+      };
+    }
+
+    const session = await getHTSession();
+    if (!session || !session.user?.email) {
+      return { success: false, error: "Не си влязъл с имейл" };
+    }
+
+    const user = (
+      await db
+        .select({ id: users.id })
+        .from(users)
+        .where(eq(users.email, session.user.email))
+    ).at(0);
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const newParticipant = (
+      await db
+        .insert(particpants)
+        .values({
+          userId: user.id,
+          firstName: data.firstName,
+          // FIXME: rename secondName to middleName or vise-versa
+          middleName: data.secondName,
+          lastName: data.lastName,
+          phoneNumber: data.phoneNumber,
+          grade: data.class.grade,
+          parallel: data.class.parallel,
+          allergies: data.allergies,
+          tShirtId: data.tShirtId,
+          technologies: data.technologies,
+          isLookingForTeam: data.isLookingForTeam,
+          question1Answer: data.question1,
+          question2Answer: data.question2,
+        })
+        .returning({ id: particpants.id })
+    ).at(0);
+    if (!newParticipant) {
+      throw new Error("Participant not found");
+    }
+    return { success: true, participantId: newParticipant.id };
+  } catch (e) {
+    // HACK: this is a hack to get the error message from the drizzle-orm error,
+    // so we can show it to the user. Shouldnt happen to sane users, but we
+    // should still handle it. Still a bad UX, but better than nothing.
+    if (
+      e instanceof Error &&
+      e.message.includes("participants_phone_number_unique")
+    ) {
+      return {
+        success: false,
+        error: "Този телефонен номер вече е регистриран.",
+      };
+    }
     return {
       success: false,
-      error: "Регистрацията на завършили не е позволена по това време.",
+      error:
+        "Възникна неочаквана грешка при регистрацията. Моля опитайте отново по-късно. Ако проблемът продължава, моля свържете се с нас.",
     };
   }
-
-  const session = await getHTSession();
-  if (!session || !session.user?.email) {
-    return { success: false, error: "Не си влязъл с имейл" };
-  }
-
-  const user = (
-    await db
-      .select({ id: users.id })
-      .from(users)
-      .where(eq(users.email, session.user.email))
-  ).at(0);
-  if (!user) {
-    throw new Error("User not found");
-  }
-
-  const newParticipant = (
-    await db
-      .insert(particpants)
-      .values({
-        userId: user.id,
-        firstName: data.firstName,
-        // FIXME: rename secondName to middleName or vise-versa
-        middleName: data.secondName,
-        lastName: data.lastName,
-        phoneNumber: data.phoneNumber,
-        grade: data.class.grade,
-        parallel: data.class.parallel,
-        allergies: data.allergies,
-        tShirtId: data.tShirtId,
-        technologies: data.technologies,
-        isLookingForTeam: data.isLookingForTeam,
-        question1Answer: data.question1,
-        question2Answer: data.question2,
-      })
-      .returning({ id: particpants.id })
-  ).at(0);
-  if (!newParticipant) {
-    throw new Error("Participant not found");
-  }
-  return { success: true, participantId: newParticipant.id };
 });
