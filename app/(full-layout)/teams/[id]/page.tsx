ebf0fb2332db9cee1a215e2fa2e660bf58bd3aea
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-12-13 20:03:59 +0200

    replaced divs with card components

--- app/(full-layout)/teams/[id]/page.tsx
+++ app/(full-layout)/teams/[id]/page.tsx
@@ -28,6 +28,7 @@ import {
 } from "~/app/components/ui/avatar";
 import { Badge } from "~/app/components/ui/badge";
 import { Button } from "~/app/components/ui/button";
+import { Card } from "~/app/components/ui/card";
 import {
   Tabs,
   TabsContent,
@@ -76,7 +77,7 @@ export default async function TeamDetailPage({
 
   return (
     <div className="h-full w-full max-w-6xl justify-center text-center ">
-      <div className="h-min rounded-3xl border-2 p-5 pt-0 sm:p-10 sm:pt-5">
+      <Card className="h-min rounded-3xl border-2 p-5 pt-0 sm:p-10 sm:pt-5">
         <div className="flex w-full">
           <div className="flex items-center">
             <Button asChild variant="secondary" className="mt-8">
@@ -119,9 +120,9 @@ export default async function TeamDetailPage({
             />
           ))}
         </div>
-      </div>
-      <div className="w-full sm:flex">
-        <div className="mt-10 self-center rounded-3xl border-2 p-3 sm:w-3/5">
+      </Card>
+      <div className="w-full  sm:flex">
+        <Card className="mt-10 self-center rounded-3xl border-2 p-3 sm:w-3/5">
           <Tabs defaultValue="information">
             {participant?.team.id == team.id && (
               <TabsList className="mb-4">
@@ -211,9 +212,9 @@ export default async function TeamDetailPage({
               </TabsContent>
             </div>
           </Tabs>
-        </div>
+        </Card>
         <div className="sm:w-2/5">
-          <div className="m-10 ml-auto mr-auto flex h-min w-5/6 rounded-3xl border-2 p-5 sm:mr-0">
+          <Card className="m-10 ml-auto mr-auto flex h-min w-5/6 rounded-3xl border-2 p-5 sm:mr-0">
             {team.mentorId && url ? (
               <>
                 <div>
@@ -237,9 +238,9 @@ export default async function TeamDetailPage({
                 <h2 className="m-auto text-lg">Все още няма ментор</h2>
               </>
             )}
-          </div>
+          </Card>
 
-          <div className="m-10 ml-auto mr-auto h-min w-5/6 overflow-hidden rounded-3xl border-2 p-5 sm:mr-0">
+          <Card className="m-10 ml-auto mr-auto h-min w-5/6 overflow-hidden rounded-3xl border-2 p-5 sm:mr-0">
             <h3 className="text-2xl">Технологии</h3>
             {techn && techn.length > 0 ? (
               <div className="w-full flex-auto gap-2 p-2">
@@ -262,7 +263,7 @@ export default async function TeamDetailPage({
                 Няма технологии :(
               </Badge>
             )}
-          </div>
+          </Card>
         </div>
       </div>
     </div>
