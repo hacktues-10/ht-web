a0fc654319703a684d3bb31e8e78eb1b9340088a
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-12-13 08:50:24 +0200

    /discord страници

---
+++ app/(full-layout)/discord/error/page.tsx
@@ -0,0 +1,13 @@
+export default function DiscordErrorPage() {
+  return (
+    <div className="mt-20 items-center justify-center text-center">
+      <h2 className="font-htags scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Възникна грешка при свързването с Discord профила
+      </h2>
+      <p className="mb-6 text-center text-lg italic leading-7 [&:not(:first-child)]:mt-6">
+        Моля опитайте отново по-късно. Ако проблемът продължава, моля свържете
+        се с нас на адрес hacktues@elsys-bg.org.
+      </p>
+    </div>
+  );
+}
