2ee9e3f7e2296d509638e4fc978552b1161df06b
bvpav <78971592+bvpav@users.noreply.github.com>
2023-12-13 21:24:32 +0200

    no longer experimental yay

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ import { env } from "./app/env.mjs";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
   images: {
     remotePatterns: [
       {
