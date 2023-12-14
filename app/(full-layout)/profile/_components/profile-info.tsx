daad89a97d4758386d6ddebda31f210eaf0fe0ed
bvpav <78971592+bvpav@users.noreply.github.com>
2023-12-14 21:23:43 +0200

    fixed wrongful names and file organization

--- app/(full-layout)/profile/_components/profile-info.tsx
+++ app/(full-layout)/profile/_components/profile-info.tsx
@@ -14,24 +14,24 @@ import {
   CommandInput,
   CommandItem,
 } from "~/app/components/ui/command";
+import { Input } from "~/app/components/ui/input";
+import { Label } from "~/app/components/ui/label";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "~/app/components/ui/popover";
-import { getParticipantFromSession } from "../participants/service";
+import { Textarea } from "~/app/components/ui/textarea";
+import { useToast } from "~/app/components/ui/use-toast";
+import { getParticipantFromSession } from "~/app/participants/service";
 import {
   convertTechnologiesToText,
   convertToTechnology,
   prepareTechnologies,
-} from "../technologies";
-import { cn } from "../utils";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
-import { Textarea } from "./ui/textarea";
-import { useToast } from "./ui/use-toast";
+} from "~/app/technologies";
+import { cn } from "~/app/utils";
 
-export default function RenderProfileInfo({
+export default function ProfileInfo({
   participant,
 }: {
   participant: Awaited<ReturnType<typeof getParticipantFromSession>>;
