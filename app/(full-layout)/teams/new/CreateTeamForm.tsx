6b8130869f209146ece327c995f1fec24a9ebb08
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2023-12-14 14:11:49 +0200

    in card component

--- app/(full-layout)/teams/new/CreateTeamForm.tsx
+++ app/(full-layout)/teams/new/CreateTeamForm.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import invariant from "tiny-invariant";
 
+import { Card } from "~/app/components/ui/card";
 import { createTeamAction } from "./actions";
 
 export function CreateTeamForm() {
@@ -28,31 +29,33 @@ export function CreateTeamForm() {
   }
 
   return (
-    <form
-      className="mx-auto flex max-w-md flex-col gap-2 rounded-lg bg-gray-100 p-4 shadow-md"
-      onSubmit={handleSubmit}
-    >
-      <h1 className="py-3 text-center text-3xl font-bold">
-        Създай нов отбор!!1!
-      </h1>
-      <input
-        type="text"
-        name="name"
-        placeholder="Име на отбора"
-        className="mb-2 w-full rounded border border-gray-300 p-2 focus:ring focus:ring-blue-200"
-        required
-      />
-      <textarea
-        name="description"
-        placeholder="Описание на отбора"
-        className="mb-2 w-full rounded border border-gray-300 p-2 focus:ring focus:ring-blue-200"
-      />
-      <button
-        type="submit"
-        className="w-full rounded bg-blue-500 p-2 text-white hover:bg-blue-600"
+    <Card>
+      <form
+        className="mx-auto flex max-w-md flex-col gap-2 rounded-l p-4 shadow-md"
+        onSubmit={handleSubmit}
       >
-        Създай отбор
-      </button>
-    </form>
+        <h1 className="py-3 text-center text-3xl font-bold">
+          Създай нов отбор!!1!
+        </h1>
+        <input
+          type="text"
+          name="name"
+          placeholder="Име на отбора"
+          className="mb-2 w-full rounded border border-gray-300 p-2 focus:ring focus:ring-blue-200"
+          required
+        />
+        <textarea
+          name="description"
+          placeholder="Описание на отбора"
+          className="mb-2 w-full rounded border border-gray-300 p-2 focus:ring focus:ring-blue-200"
+        />
+        <button
+          type="submit"
+          className="w-full rounded bg-blue-500 p-2 text-white hover:bg-blue-600"
+        >
+          Създай отбор
+        </button>
+      </form>
+    </Card>
   );
 }
