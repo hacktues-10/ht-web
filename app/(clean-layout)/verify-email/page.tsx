e4b798f62d12aa7b256d8597e93316f63e847703
bvpav <bvpav@tutanota.com>
2023-12-19 21:10:19 +0200

    added verify-request page

---
+++ app/(clean-layout)/verify-email/page.tsx
@@ -0,0 +1,22 @@
+import { HTXLogoDuotone } from "~/app/components/logos";
+import { Card } from "~/app/components/ui/card";
+import { Separator } from "~/app/components/ui/separator";
+
+export default function VerifyEmailPage() {
+  return (
+    <section className="flex w-full max-w-lg flex-col gap-5">
+      <Card className="flex w-full flex-col gap-3 p-6">
+        <h1 className="text-center text-3xl font-extrabold">
+          Потвърдете имейл адреса си!
+        </h1>
+        <p className="text-center">
+          Моля, проверете имейл адреса си за линк за потвърждение.
+        </p>
+      </Card>
+      <Separator />
+      <p className="cursor-default text-center text-xl">
+        <HTXLogoDuotone />
+      </p>
+    </section>
+  );
+}
