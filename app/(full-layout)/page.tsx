8a65fa4fe8e7e91c2dd457bccec435423032aed6
bvpav <bvpav@tutanota.com>
2023-12-29 23:56:32 +0200

    added date displays and more sections

--- app/(full-layout)/page.tsx
+++ app/(full-layout)/page.tsx
@@ -20,18 +20,19 @@ import {
   STUDENTS_REGISTRATION_START,
 } from "~/app/_configs/hackathon";
 import { Hackathon, HACKATHONS } from "../_configs/archive";
-import { MediaArticle, Podkrepqsht } from "../_configs/podkrepq";
 import {
-  IfAllHTFeaturesOff,
-  IfAnyHTFeatureOn,
-  IfHTFeatureOn,
-} from "../_integrations/components";
+  MEDIA_ARTICLES,
+  MediaArticle,
+  Podkrepqsht,
+} from "../_configs/podkrepq";
+import { IfAnyHTFeatureOn } from "../_integrations/components";
 import { CountdownTimer } from "../components/countdowns";
+import { DateDisplay, DateRangeDisplay } from "../components/date-display";
 import { CountdownHourglass } from "../components/hourglass";
 import { HTXLogoDuotone } from "../components/logos";
 import { Button } from "../components/ui/button";
 import { Card, CardFooter, CardHeader, CardTitle } from "../components/ui/card";
-import { cn, FEBRUARY, JANUARY } from "../utils";
+import { cn } from "../utils";
 
 export default async function LandingPage() {
   return (
@@ -96,7 +97,7 @@ export default async function LandingPage() {
               <HTXLogoDuotone />
             </h2>
             <p className="scroll-m-20 pb-2 font-lazydog text-xl font-extrabold italic tracking-tight first:mt-0">
-              „Десетото юбилейно издание на емблематичния за ТУЕС хакатон!“
+              „Десетото юбилейно издание на емблематичния за ТУЕС хакатон“
             </p>
           </div>
           <ArchiveLocation
@@ -112,30 +113,24 @@ export default async function LandingPage() {
             <p className="text-lg">
               За първи път в историята на Hack TUES могат да участват завършили
               ученици.
-              <ArchiveStatsCard className="md:grid-cols-2">
-                <p className="col-span-2 pb-3 text-center text-2xl font-semibold">
-                  Но местата са ограничени!
-                </p>
-                <ArchiveStatsItem value={80} label="отбора на ученици" />
-                <ArchiveStatsItem value={10} label="отбора на завършили" />
-                <p className="text-md col-span-2 text-center font-light">
-                  Регистрацията <em className="italic">затваря окончателно</em>{" "}
-                  на{" "}
-                  {dateFormatter.format(
-                    fiveDaysAfter(ALUMNI_REGISTRATION_START),
-                  )}{" "}
-                  за завършили и на{" "}
-                  {dateFormatter.format(
-                    fiveDaysAfter(STUDENTS_REGISTRATION_START),
-                  )}{" "}
-                  за ученици, или{" "}
-                  <strong className="font-extrabold">
-                    до изчерпване на местата
-                  </strong>
-                  .
-                </p>
-              </ArchiveStatsCard>{" "}
             </p>
+            <ArchiveStatsCard className="md:grid-cols-2">
+              <p className="col-span-2 pb-3 text-center text-2xl font-semibold">
+                Но местата са ограничени!
+              </p>
+              <ArchiveStatsItem value={80} label="отбора на ученици" />
+              <ArchiveStatsItem value={10} label="отбора на завършили" />
+              <p className="text-md col-span-2 text-center font-light">
+                Регистрацията <em className="italic">затваря окончателно</em> на{" "}
+                <DateDisplay date={ALUMNI_REGISTRATION_START} /> за завършили и
+                на <DateDisplay date={STUDENTS_REGISTRATION_START} /> за
+                ученици, или{" "}
+                <strong className="font-extrabold">
+                  до изчерпване на местата
+                </strong>
+                .
+              </p>
+            </ArchiveStatsCard>{" "}
           </div>
           <IfAnyHTFeatureOn outOf={["register-alumni", "register-students"]}>
             <div className="flex flex-col items-center justify-center gap-4 text-center">
@@ -149,11 +144,12 @@ export default async function LandingPage() {
           </IfAnyHTFeatureOn>
         </ArchiveSection>
       </ArchiveRoot>
-      {/* <section className="relative flex flex-col items-center gap-3 pt-7">
-        <div
+      <section className="relative flex flex-col items-center gap-3 pt-7">
+        {/* TODO: put it on mitko's thing */}
+        {/* <div
           className="absolute -top-3 left-1/2 -z-30 h-[34px] min-h-[800px] w-[300%] -translate-x-1/2 bg-[radial-gradient(ellipse_50%_50%_at_50%_0%,hsl(var(--sand)/0.15),transparent)]"
           aria-hidden
-        />
+        /> */}
         <h2 className="scroll-m-20 pt-7 text-center text-5xl font-extrabold tracking-tight first:mt-0">
           Медиите за нас
         </h2>
@@ -161,7 +157,16 @@ export default async function LandingPage() {
         {MEDIA_ARTICLES.map((article) => (
           <MediaArticleCard key={article.title} article={article} />
         ))}
-      </section> */}
+      </section>
+      <section className="relative flex flex-col items-center gap-3 pt-7">
+        <h2 className="scroll-m-20 pt-7 text-center text-5xl font-extrabold tracking-tight first:mt-0">
+          Какво чакате?
+        </h2>
+        <div className="py-2" />
+        <Link href="/signup">
+          <Button size="lg">Регистрирайте се!</Button>
+        </Link>
+      </section>
       <div className="pb-4"></div>
     </div>
   );
@@ -195,24 +200,26 @@ function CountdownHero() {
   return (
     <div className="mx-auto flex min-h-fit w-full max-w-sm flex-col items-center justify-between gap-10 md:max-w-5xl md:flex-row">
       <section className="flex h-full w-full flex-col items-center justify-center gap-4">
-        <h1 className="relative inline-block bg-gradient-to-br from-[#ce0e3eff] to-[#e3686bff] bg-clip-text text-center font-llpixel text-5xl text-transparent md:text-7xl">
-          <div
-            className="absolute left-1/2 top-1/2 -z-30 h-[300%] min-h-[800px] w-[300%] -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(ellipse_50%_50%_at_50%_50%,#ce0e3e1c,transparent)]"
-            aria-hidden
-          />
-          <PageBackdrop className="top-1/2 h-[300%] min-h-[800px] -translate-y-1/2 bg-[radial-gradient(ellipse_50%_50%_at_50%_50%,#ce0e3e1c,transparent)]" />
-          Hack TUES&nbsp;
-          <span className="bg-[radial-gradient(farthest-corner_at_100%_50%,#ffdea6a3_3%,transparent_45%)] bg-clip-text text-transparent">
-            X
-          </span>
-        </h1>
-        <p className="lg:text-md text-center font-lazydog text-xs">
-          Eмблематичният за ТУЕС хакатон се&nbsp;завръща!
-        </p>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="relative inline-block bg-gradient-to-br from-[#ce0e3eff] to-[#e3686bff] bg-clip-text text-center font-llpixel text-5xl text-transparent md:text-7xl">
+            <div
+              className="absolute left-1/2 top-1/2 -z-30 h-[300%] min-h-[800px] w-[300%] -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(ellipse_50%_50%_at_50%_50%,#ce0e3e1c,transparent)]"
+              aria-hidden
+            />
+            <PageBackdrop className="top-1/2 h-[300%] min-h-[800px] -translate-y-1/2 bg-[radial-gradient(ellipse_50%_50%_at_50%_50%,#ce0e3e1c,transparent)]" />
+            Hack TUES&nbsp;
+            <span className="bg-[radial-gradient(farthest-corner_at_100%_50%,#ffdea6a3_3%,transparent_45%)] bg-clip-text text-transparent">
+              X
+            </span>
+          </h1>
+          <p className="text-center font-lazydog text-sm lg:text-lg">
+            Eмблематичният за ТУЕС хакатон се&nbsp;завръща!
+          </p>
+        </div>
 
-        <div className="flex flex-col gap-1 text-sm font-semibold">
+        <div className="flex flex-col gap-1 py-3 text-sm font-semibold">
           <IconParagraph icon={Calendar}>
-            {dateFormatter.formatRange(EVENT_START, EVENT_END)}
+            <DateRangeDisplay startDate={EVENT_START} endDate={EVENT_END} />
           </IconParagraph>
           <IconParagraph icon={MapPin}>София Тех Парк</IconParagraph>
         </div>
@@ -272,12 +279,6 @@ function PodkrepqTitle({ children }: PropsWithChildren<{}>) {
   );
 }
 
-const dateFormatter = new Intl.DateTimeFormat("bg", {
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-});
-
 function MediaArticleCard({ article }: { article: MediaArticle }) {
   return (
     <Link href={article.url} className="block" target="_blank">
@@ -297,9 +298,7 @@ function MediaArticleCard({ article }: { article: MediaArticle }) {
           </CardHeader>
           <div className="pb-3 text-center sm:hidden">{"●"}</div>
           <CardFooter className="justify-center text-center sm:justify-start sm:text-left">
-            <time dateTime={article.date.toISOString()}>
-              {dateFormatter.format(article.date)}
-            </time>
+            <DateDisplay date={article.date} />
           </CardFooter>
         </div>
       </Card>
@@ -374,7 +373,7 @@ function ArchiveLocation({
   return (
     <div className="flex flex-col gap-2 sm:flex-row sm:justify-between sm:gap-4">
       <IconParagraph icon={Calendar}>
-        {dateFormatter.formatRange(startDate, endDate)}
+        <DateRangeDisplay startDate={startDate} endDate={endDate} />
       </IconParagraph>
       {location ? (
         <IconParagraph icon={MapPin}>{location}</IconParagraph>
