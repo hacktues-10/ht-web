42048b8753b4f2397ddd2d451a1bbd5dbc91c0f6
bvpav <bvpav@tutanota.com>
2023-12-31 19:58:36 +0200

    improved discord flow error handling

--- app/(clean-layout)/discord/error/page.tsx
+++ app/(clean-layout)/discord/error/page.tsx
@@ -27,13 +27,8 @@ export default function DiscordErrorPage({
 
   return (
     <div className="mt-20 max-w-lg items-center justify-center text-center">
-<<<<<<< HEAD
-      <h2 className="scroll-m-20  border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-        Възникна грешка с Discord профила
-=======
-      <h2 className="scroll-m-20  border-b pb-2 font-htags text-3xl font-semibold tracking-tight first:mt-0">
+      <h2 className="font-htags  scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
         Нещо се обърка
->>>>>>> ee67f8c (improved discord flow error handling)
       </h2>
       <p className="mb-6 text-center text-lg leading-7 [&:not(:first-child)]:mt-6">
         Възникна неочаквана грешка при комуникацията с Discord. Моля, опитайте
