f728b21c0835a23e22ee31f73d184c5cbbee1339
bvpav <bvpav@tutanota.com>
2024-01-01 12:30:54 +0200

    fixed error message

--- app/(clean-layout)/discord/error/page.tsx
+++ app/(clean-layout)/discord/error/page.tsx
@@ -4,9 +4,9 @@ import { redirect } from "next/navigation";
 import { DiscordLoadingLink } from "../_components/discord-loading-link";
 
 export default function DiscordErrorPage({
-  searchParams: { source, q: obfuscatedErrorCode },
+  searchParams: { source, q: obfuscatedErrorCode, t: obfuscatedTimestamp },
 }: {
-  searchParams: { source?: string; q?: string };
+  searchParams: { source?: string; q?: string; t?: string };
 }) {
   const redirectUrl =
     source === "/discord"
@@ -20,21 +20,24 @@ export default function DiscordErrorPage({
     redirect(redirectUrl);
   }
 
-  const errorCode = obfuscatedErrorCode && getErrorCode(obfuscatedErrorCode);
+  const errorCode =
+    obfuscatedErrorCode &&
+    obfuscatedTimestamp &&
+    getErrorCode(obfuscatedErrorCode, obfuscatedTimestamp);
 
   return (
     <div className="mt-20 max-w-lg items-center justify-center text-center">
       <h2 className="scroll-m-20  border-b pb-2 font-htags text-3xl font-semibold tracking-tight first:mt-0">
         Нещо се обърка
       </h2>
-      <p className="mb-6 text-center text-lg italic leading-7 [&:not(:first-child)]:mt-6">
-        Възникна неочакване грешка при комуникацията с Discord. Моля опитайте
+      <p className="mb-6 text-center text-lg leading-7 [&:not(:first-child)]:mt-6">
+        Възникна неочаквана грешка при комуникацията с Discord. Моля, опитайте
         отново.
       </p>
       <DiscordLoadingLink href={redirectUrl}>Опитай отново</DiscordLoadingLink>
       <div className="my-9 space-y-3 text-sm text-muted-foreground">
         <p className="text-center italic">
-          Ако проблемът продължава, моля свържете се с нас на адрес{" "}
+          Ако проблемът продължава, моля, свържете се с нас на адрес{" "}
           <Link href="mailto:hacktues@elsys-bg.org" className="underline">
             hacktues@elsys-bg.org
           </Link>
@@ -43,7 +46,8 @@ export default function DiscordErrorPage({
         {!!errorCode && (
           <>
             <p className="text-center italic">
-              Когато се свързвате с нас, моля следния код на грешката:
+              Когато се свързвате с нас, моля, споделете следния код на
+              грешката:
             </p>
             <p className="text-md text-center italic">
               <code>HT-{errorCode}</code>
@@ -55,13 +59,23 @@ export default function DiscordErrorPage({
   );
 }
 
-function getErrorCode(obfuscatedErrorCode: string) {
+function getErrorCode(
+  obfuscatedErrorCode: string,
+  obfuscatedTimestamp: string,
+) {
   try {
     const errorCode = parseInt(obfuscatedErrorCode, 36);
     if (isNaN(errorCode)) {
       return null;
     }
-    return btoa(`${errorCode}:${obfuscatedErrorCode}`).replace(/=/g, "");
+    const timestamp = parseInt(obfuscatedTimestamp, 36);
+    if (isNaN(timestamp)) {
+      return null;
+    }
+    const transform = (str: string) => str.split("").reverse().join("");
+    return btoa(
+      transform(obfuscatedErrorCode) + ":" + transform(obfuscatedTimestamp),
+    ).replace(/=/g, "");
   } catch {
     return null;
   }
