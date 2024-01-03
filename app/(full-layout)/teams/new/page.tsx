2be1188d7942b6c8f254045377d9e57131ce831b
bvpav <bvpav@tutanota.com>
2024-01-04 01:57:52 +0200

    testing on and off

--- app/(full-layout)/teams/new/page.tsx
+++ app/(full-layout)/teams/new/page.tsx
@@ -8,26 +8,32 @@ import { CreateTeamForm } from "./CreateTeamForm";
 
 export default async function CreateTeamPage() {
   const gb = await getServerSideGrowthBook();
-  if (gb.isOff("create-team")) {
-    notFound();
-  }
-
-  const session = await getHTSession();
-  if (!session) {
-    signInRedirect();
-  }
-
   return (
-    <div className="flex flex-col justify-center">
-      <div className="w-min self-center pb-4">
-        <Button asChild variant="secondary" className="mt-8 ">
-          <Link href="/teams">
-            {"<- "}
-            Назад
-          </Link>
-        </Button>
-      </div>
-      <CreateTeamForm />
-    </div>
+    <>
+      <p>{JSON.stringify(gb.isOn("create-team"))}</p>
+      <p>{JSON.stringify(gb.isOff("create-team"))}</p>
+    </>
   );
+  // if (gb.isOff("create-team")) {
+  //   notFound();
+  // }
+
+  // const session = await getHTSession();
+  // if (!session) {
+  //   signInRedirect();
+  // }
+
+  // return (
+  //   <div className="flex flex-col justify-center">
+  //     <div className="w-min self-center pb-4">
+  //       <Button asChild variant="secondary" className="mt-8 ">
+  //         <Link href="/teams">
+  //           {"<- "}
+  //           Назад
+  //         </Link>
+  //       </Button>
+  //     </div>
+  //     <CreateTeamForm />
+  //   </div>
+  // );
 }
