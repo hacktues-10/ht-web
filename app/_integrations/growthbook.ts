82345e93e418eaa24ceb63b6e633c4c4b2bb3de4
bvpav <78971592+bvpav@users.noreply.github.com>
2024-01-05 00:29:53 +0200

    maybe?

--- app/_integrations/growthbook.ts
+++ app/_integrations/growthbook.ts
@@ -1,4 +1,5 @@
 import { cache } from "react";
+import { unstable_cache } from "next/cache";
 import { GrowthBook } from "@growthbook/growthbook";
 
 import { HTFeatures } from "~/app/_context/growthbook/features";
@@ -7,15 +8,21 @@ import { env } from "../env.mjs";
 // FIXME: do we need to use user id:
 // https://github.com/esauri/growthbook-next-app-router/blob/main/src/utils/growthbook.ts
 // XXX: should we move this to _integrations/growthbook.ts? (or some subdirectory of _integrations)
-export const getServerSideGrowthBook = cache(async (id?: string) => {
-  const growthbook = new GrowthBook<HTFeatures>({
-    apiHost: env.NEXT_PUBLIC_GROWTHBOOK_API_HOST,
-    clientKey: env.NEXT_PUBLIC_GROWTHBOOK_CLIENT_KEY,
-    enableDevMode: env.VERCEL_ENV === "development",
-    attributes: {
-      id,
-    },
-  });
-  await growthbook.loadFeatures({ timeout: 1000 });
-  return growthbook;
-});
+export const getServerSideGrowthBook = unstable_cache(
+  cache(async (id?: string) => {
+    const growthbook = new GrowthBook<HTFeatures>({
+      apiHost: env.NEXT_PUBLIC_GROWTHBOOK_API_HOST,
+      clientKey: env.NEXT_PUBLIC_GROWTHBOOK_CLIENT_KEY,
+      enableDevMode: env.VERCEL_ENV === "development",
+      attributes: {
+        id,
+      },
+    });
+    await growthbook.loadFeatures({ timeout: 1000 });
+    return growthbook;
+  }),
+  ["server-side-growthbook"],
+  {
+    revalidate: 60, // 1 minute
+  },
+);
