9eb5e31ee5c5394f9fae37f4d8d1ff5876ea102d
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-01-05 16:04:00 +0200

    mai mai rabotiiiii izi izi

--- app/(clean-layout)/signup/page.tsx
+++ app/(clean-layout)/signup/page.tsx
@@ -1,13 +1,32 @@
 import Link from "next/link";
 
 import { IfAnyHTFeatureOn } from "~/app/_integrations/components";
-import { HTXLogoDuotone } from "~/app/components/logos";
+import { HTLogo, HTXLogoDuotone } from "~/app/components/logos";
 import { Card } from "~/app/components/ui/card";
 import { Separator } from "~/app/components/ui/separator";
+import { getParticipantFromSession } from "~/app/participants/service";
 import { RegistrationCountdownOverlay } from "../_components/countdown-overlay";
 import { SignInForm } from "../_components/signin-form";
 
 export default async function SignUpPage() {
+  const participant = await getParticipantFromSession();
+  if (participant) {
+    return (
+      <section className="flex w-full max-w-sm flex-col gap-5">
+        <h1 className="text-center text-3xl font-extrabold">
+          Вече сте влезли в акаунта си!
+        </h1>
+        <h2 className="text-center text-xl font-semibold">
+          Върнете се към началната страница
+        </h2>
+        <p className="text-center text-xl">
+          <Link href="/">
+            <HTLogo>Начална страница</HTLogo>
+          </Link>
+        </p>
+      </section>
+    );
+  }
   return (
     <section className="flex w-full max-w-sm flex-col gap-5">
       <h1 className="text-center text-3xl font-extrabold">Регистрация</h1>
