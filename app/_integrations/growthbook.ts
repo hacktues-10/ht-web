59b64cef31f503ee8058d2552ac522dd3505ec72
bvpav <bvpav@tutanota.com>
2024-01-07 12:25:58 +0200

    this shouldnt store now...

--- app/_integrations/growthbook.ts
+++ app/_integrations/growthbook.ts
@@ -1,4 +1,5 @@
 import { cache } from "react";
+import { unstable_noStore } from "next/cache";
 import { GrowthBook } from "@growthbook/growthbook";
 
 import { HTFeatures } from "~/app/_context/growthbook/features";
@@ -7,7 +8,8 @@ import { env } from "../env.mjs";
 // FIXME: do we need to use user id:
 // https://github.com/esauri/growthbook-next-app-router/blob/main/src/utils/growthbook.ts
 // XXX: should we move this to _integrations/growthbook.ts? (or some subdirectory of _integrations)
-export const getServerSideGrowthBook = cache(async (id?: string) => {
+export const getServerSideGrowthBook = async (id?: string) => {
+  unstable_noStore(); // XXX: Това може да чупи всичко, но пък може и да не чупи
   const growthbook = new GrowthBook<HTFeatures>({
     apiHost: env.NEXT_PUBLIC_GROWTHBOOK_API_HOST,
     clientKey: env.NEXT_PUBLIC_GROWTHBOOK_CLIENT_KEY,
@@ -18,4 +20,4 @@ export const getServerSideGrowthBook = cache(async (id?: string) => {
   });
   await growthbook.loadFeatures({ timeout: 1000 });
   return growthbook;
-});
+};
