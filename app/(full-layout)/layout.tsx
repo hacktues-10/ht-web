bca74de88f2a294157a4cb343c22d41a0ffc7f0b
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-01-08 14:08:33 +0200

    added new line

--- app/(full-layout)/layout.tsx
+++ app/(full-layout)/layout.tsx
@@ -1,4 +1,5 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
+
 import { Footer } from "../components/footer";
 import { Header } from "../components/header/header";
 import { Separator } from "../components/ui/separator";
