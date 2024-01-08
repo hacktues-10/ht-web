841fbb9c7d67b813dd9ef4c8fe1714c453ce7c00
bvpav <bvpav@tutanota.com>
2024-01-08 02:47:28 +0200

    made it slower

--- app/(full-layout)/page.tsx
+++ app/(full-layout)/page.tsx
@@ -413,7 +413,7 @@ function UnescoSection() {
             <Image
               src={ht8Image}
               alt="Екипът на Hack TUES 8 гледа в екрана на лаптоп"
-              className="h-auto w-full rounded-lg object-cover object-center shadow-md transition-all hover:scale-105 hover:shadow-xl"
+              className="h-auto w-full rounded-lg object-cover object-center shadow-md transition-all duration-300 hover:scale-105 hover:shadow-xl"
             />
           </Link>
         </div>
