7335c60bd12d4cd371fb6fffb47b502e6b51c678
bvpav <78971592+bvpav@users.noreply.github.com>
2024-01-08 12:02:03 +0200

    improved discord success page significantly

--- app/(clean-layout)/discord/page.tsx
+++ app/(clean-layout)/discord/page.tsx
@@ -1,9 +1,13 @@
 import { Metadata } from "next";
+import Link from "next/link";
 import { XIcon } from "lucide-react";
 import { FaDiscord } from "react-icons/fa6";
 
+import { SOCIAL_MEDIA } from "~/app/_configs/pr";
+import { IfHTFeatureOn } from "~/app/_integrations/components";
 import { getUserAuthorization, signInRedirect } from "~/app/api/auth/session";
 import { HTLogo } from "~/app/components/logos";
+import { Button } from "~/app/components/ui/button";
 import { Card } from "~/app/components/ui/card";
 import { Separator } from "~/app/components/ui/separator";
 import { DiscordLoadingLink } from "./_components/discord-loading-link";
@@ -17,19 +21,60 @@ export default async function DiscordPage() {
   const { hasSession, hasConnectedDiscord } = await getUserAuthorization();
   if (!hasSession) signInRedirect();
 
+  const socialMedia = SOCIAL_MEDIA.filter(
+    (s) => s.platform !== "Email" && s.platform !== "Linktree",
+  );
+  const randomSocialMedia =
+    socialMedia[Math.floor(Math.random() * socialMedia.length)];
+
   return hasConnectedDiscord ? (
-    <Card className="mt-20 p-5">
+    <Card className="mt-20 px-2 py-5 sm:px-5">
       <div className="max-w-lg items-center justify-center text-center">
-        <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        <div className="flex items-center justify-center">
+          <FaDiscord className="pb-5 text-8xl" />
+        </div>
+        <h1 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0">
           Успешно свързахте своя Discord профил!
         </h1>
+        <h2 className="text-center text-2xl leading-7 [&:not(:first-child)]:mt-6">
+          Какво следва?
+        </h2>
+        <div className="px-0 py-3 sm:px-8">
+          <div className="flex flex-col gap-2 border-2 p-3">
+            <IfHTFeatureOn feature="create-team">
+              <Button asChild variant="secondary">
+                <Link href="/teams/new">Създайте нов отбор</Link>
+              </Button>
+            </IfHTFeatureOn>
+            <IfHTFeatureOn feature="show-teams">
+              <Button asChild variant="secondary">
+                <Link href="/teams">Разгледайте отборите</Link>
+              </Button>
+            </IfHTFeatureOn>
+            <Button asChild variant="secondary">
+              <Link href="/profile">Персонализирайте профила си</Link>
+            </Button>
+            <Button asChild variant="secondary">
+              <Link href={randomSocialMedia.link} target="_blank">
+                Последвайте ни в {randomSocialMedia.platform}
+              </Link>
+            </Button>
+            <Button asChild variant="secondary">
+              <Link href="/">Към началната страница</Link>
+            </Button>
+          </div>
+        </div>
+        <div className="pb-2" />
         <Separator />
         <p className="text-center text-sm leading-7 [&:not(:first-child)]:mt-6">
-          Можете да премахнете своя Discord профил от{" "}
-          <HTLogo className="text-inherit">Hack&nbsp;TUES&nbsp;X</HTLogo>.
+          Грешен Discord профил?
         </p>
-        <div className="py-2" />
-        <DiscordLoadingLink href="/api/discord/remove" variant="destructive">
+        <div className="py-1" />
+        <DiscordLoadingLink
+          href="/api/discord/remove"
+          variant="destructive"
+          size="sm"
+        >
           <XIcon className="mr-2 inline-block h-4 w-4" /> Премахни Discord
         </DiscordLoadingLink>
       </div>
