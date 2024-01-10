7a9d6eff87417b64f8e1633852d8ed9c47070d3e
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-01-10 21:15:38 +0200

    ne baca oshte

--- app/(full-layout)/ourteam/components/organizatorDetailed.tsx
+++ app/(full-layout)/ourteam/components/organizatorDetailed.tsx
@@ -4,10 +4,22 @@ import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+import { Button } from "~/app/components/ui/button";
 import { Card } from "~/app/components/ui/card";
 import kiki from "../team/kiki.webp";
 
 import "./animations.css";
+
+import { HoverCardTrigger } from "@radix-ui/react-hover-card";
+import { Instagram } from "lucide-react";
+
+import { HoverCard, HoverCardContent } from "~/app/components/ui/hover-card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/app/components/ui/tooltip";
 
 interface Member {
   name: string;
@@ -37,40 +49,54 @@ export default function OrganizatorDetailed({
   }, []);
 
   return (
-    <Card
-      className="fadeIn z-2 m-8 mx-auto w-[350px] rounded-xl  border-2 transition-transform duration-300 ease-in-out hover:scale-105 sm:flex sm:h-[440px] md:w-[700px] lg:w-[960px]"
-      style={{
-        animationDelay: `${delay}ms`,
-        opacity: isVisibleImg ? 1 : 0,
-      }}
-    >
-      <Image
-        src={member.photo}
-        alt={member.name}
-        className={`sm:w-200 md:w-300 lg:w-400 m-auto rounded ${member.customClass}`}
-      />
-      <div className="mb-4 mt-2 w-full">
-        <p className="pt-2 font-sans text-xl font-semibold leading-7">
-          {member.name}
-        </p>
-        <p className="p-1 font-sans italic leading-7">{member.role}</p>
-        <Link
-          href={`https://www.instagram.com/${
-            member.Instagram.startsWith("@")
-              ? member.Instagram.slice(1)
-              : member.Instagram
-          }`}
-          className="w-full hover:underline"
+    <div className="h-[560px]  w-[350px]  hover:[transform:rotateY(180deg)]">
+      <Card
+        className="fadeIn z-2 [transition:0.8s_transform-style:preserve-3d] relative m-8
+          mx-auto h-[100%] w-[100%] rounded-xl border-2 text-center [transform:rotateY(180deg)]  hover:scale-105"
+      >
+        <div
+          id="front"
+          className="absolute h-full w-full [-webkit-backface-visibility:hidden_backface-visibilityhidden]"
         >
-          {member.Instagram}
-        </Link>
-        <p
-          className={`fadeIn } m-5 p-1 font-sans leading-7 text-opacity-0 transition-transform duration-300
-          ease-in-out`}
+          <p className="pt-2 font-sans text-xl font-semibold leading-7">
+            {member.name}
+          </p>
+          <p className="p-1 font-sans text-lg italic leading-7">
+            {member.role}
+          </p>
+          <div className="flex items-center">
+            <Image
+              src={member.photo}
+              alt={member.name}
+              className={`sm:w-200 md:w-300 lg:w-400 my-auto rounded ${member.customClass}`}
+            />
+          </div>
+        </div>
+        <div
+          id="back"
+          className="absolute h-full w-full [transform:rotateY(180deg)] [-webkit-backface-visibility:hidden_backface-visibilityhidden]"
         >
-          {member.description}
-        </p>
-      </div>
-    </Card>
+          <p>{member.description}</p>
+          <Button
+            size="icon"
+            variant="ghost"
+            asChild
+            className="transition-transform hover:scale-110"
+          >
+            <Link
+              href={`https://www.instagram.com/${
+                member.Instagram.startsWith("@")
+                  ? member.Instagram.slice(1)
+                  : member.Instagram
+              }`}
+              target="_blank"
+              className="hover:scale-110"
+            >
+              <Instagram />
+            </Link>
+          </Button>
+        </div>
+      </Card>
+    </div>
   );
 }
