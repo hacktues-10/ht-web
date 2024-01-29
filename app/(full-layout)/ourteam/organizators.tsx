75785a62ec02baf23ce140f1412c3c5be742654a
dimitarNzhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-01-29 13:00:27 +0200

    opraveni sa edni raboti

--- app/(full-layout)/ourteam/organizators.tsx
+++ app/(full-layout)/ourteam/organizators.tsx
@@ -65,6 +65,25 @@ export const organizators = {
       color: "#a8d4bb",
     },
   ],
+  "Екип Логистика": [
+    {
+      name: "Томи",
+      photo: tomi_r,
+      Instagram: "@therealx_ray",
+      role: "Екип Логистика",
+      customClass: "ml-[50px] mt-[30px]",
+      description: (
+        <blockquote>
+          <p>
+            Тялото ми е някъде, душата другaде, а ментално здраве никога не съм
+            имал!
+          </p>
+          <footer>- Томи</footer>
+        </blockquote>
+      ),
+      color: "#9cd1f1",
+    },
+  ],
   "Екип ИТ": [
     {
       name: "Митко",
@@ -72,7 +91,12 @@ export const organizators = {
       Instagram: "@dimitarzhv",
       role: "Екип ИТ",
       customClass: "",
-      description: <p>Не спя, за да пиша код. Тренирам, когато не пиша код. Мечтая си за Porsche като си нямам работа :)</p>,
+      description: (
+        <p>
+          Не спя, за да пиша код. Тренирам, когато не пиша код. Мечтая си за
+          Porsche като си нямам работа :)
+        </p>
+      ),
       color: "#a4f85c",
     },
     {
@@ -117,23 +141,50 @@ export const organizators = {
       color: "#bc8be4",
     },
   ],
-  "Екип Логистика": [
+  "Eкип Дизайн": [
     {
-      name: "Томи",
-      photo: tomi_r,
-      Instagram: "@therealx_ray",
-      role: "Екип Логистика",
-      customClass: "ml-[50px] mt-[30px]",
+      name: "Мечо",
+      photo: mecho,
+      Instagram: "@mechkarov_",
+      role: "Екип Дизайн",
+      customClass: "mt-[30px] ml-[30px]",
+      description: (
+        <>
+          <p>
+            Обичам гъски, гъските са топ и ако не сте съгласни с мен...
+            <br />
+            Мечо Младши ще Ви намери...
+          </p>
+          <MechoMladshi className="px-10 py-5" />
+        </>
+      ),
+      color: "#ffbf9f",
+    },
+    {
+      name: "Ели",
+      photo: eli,
+      Instagram: "@eli_trynna_stalk_u",
+      role: "Екип Дизайн",
+      customClass: "mt-[50px] ",
+      description: "Nah, I'd win",
+      color: "#f493f4",
+    },
+    {
+      name: "Коко",
+      photo: koko,
+      Instagram: "@izgubih_se_v_gorata",
+      role: "Екип Дизайн",
+      customClass: "mt-[50px] ml-[40px]",
       description: (
         <blockquote>
           <p>
-            Тялото ми е някъде, душата другaде, а ментално здраве никога не съм
-            имал!
+            Уважаеми зрители, това беше всичко от нас за тази вечер. Благодарим
+            ви, че бяхте с нас и до нови срещи!
           </p>
-          <footer>- Томи</footer>
+          <footer>- С. Трифонов</footer>
         </blockquote>
       ),
-      color: "#9cd1f1",
+      color: "#a1a1a3",
     },
   ],
   "Екип Спонсори": [
@@ -181,52 +232,6 @@ export const organizators = {
         <p>Обичам да се самозалъгвам, че ще си легна след още 5 минути.</p>
       ),
       color: "#7de4e9",
-    },
-  ],
-  "Eкип Дизайн": [
-    {
-      name: "Мечо",
-      photo: mecho,
-      Instagram: "@mechkarov_",
-      role: "Екип Дизайн",
-      customClass: "mt-[30px] ml-[30px]",
-      description: (
-        <>
-          <p>
-            Обичам гъски, гъските са топ и ако не сте съгласни с мен...
-            <br />
-            Мечо Младши ще Ви намери...
-          </p>
-          <MechoMladshi className="p-10" />
-        </>
-      ),
-      color: "#ffbf9f",
-    },
-    {
-      name: "Ели",
-      photo: eli,
-      Instagram: "@eli_trynna_stalk_u",
-      role: "Екип Дизайн",
-      customClass: "mt-[50px] ",
-      description: "Nah, I'd win",
-      color: "#f493f4",
-    },
-    {
-      name: "Коко",
-      photo: koko,
-      Instagram: "@izgubih_se_v_gorata",
-      role: "Екип Дизайн",
-      customClass: "mt-[50px] ml-[40px]",
-      description: (
-        <blockquote>
-          <p>
-            Уважаеми зрители, това беше всичко от нас за тази вечер. Благодарим
-            ви, че бяхте с нас и до нови срещи!
-          </p>
-          <footer>- С. Трифонов</footer>
-        </blockquote>
-      ),
-      color: "#a1a1a3",
     },
   ],
 };
