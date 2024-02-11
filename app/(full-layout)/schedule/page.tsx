2633fdad0096e1dacbdf22263ee6bee7f4184261
bvpav <bvpav@tutanota.com>
2024-02-11 22:37:40 +0200

    Styled schedule page

--- app/(full-layout)/schedule/page.tsx
+++ app/(full-layout)/schedule/page.tsx
@@ -1,22 +1,51 @@
 import { SCHEDULE } from "~/app/_configs/hackathon";
-import { ComingSoonPage } from "~/app/components/coming-soon/coming-soon-page";
-import { Card } from "~/app/components/ui/card";
+import { Badge } from "~/app/components/ui/badge";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/app/components/ui/card";
 
 export default function OurTeamPage() {
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex max-w-xl flex-col  items-center justify-center gap-3">
       {SCHEDULE.map((event, index) => (
-        <div className="flex gap-1" key={index}>
-          <Card asChild>
+        <div className="flex w-full gap-3" key={index}>
+          <Card asChild className="flex items-center justify-center">
             <time dateTime={event.startDate.toISOString()}>
-              <h2>{dayOfMonthFormatter.format(event.startDate)}</h2>
-              <p>{monthFormatter.format(event.startDate)}</p>
+              <CardHeader className="flex aspect-square flex-col items-center justify-center">
+                <h2 className="text-4xl font-extrabold">
+                  {dayOfMonthFormatter.format(event.startDate)}
+                </h2>
+                <p className="font-semibold">
+                  {monthFormatter.format(event.startDate)}
+                </p>
+              </CardHeader>
             </time>
           </Card>
-          <Card asChild>
+          <Card asChild className="flex w-full flex-1 flex-grow">
             <article>
-              <h1>{event.title}</h1>
-              <p>{event.description}</p>
+              <div className="flex-1">
+                <CardHeader>
+                  <CardTitle>{event.title}</CardTitle>
+                  <CardDescription>{event.description}</CardDescription>
+                </CardHeader>
+              </div>
+              <CardFooter
+                className="flex h-full w-32 flex-shrink-0 flex-grow-0 flex-col items-center justify-center gap-2"
+                asChild
+              >
+                <time dateTime={event.startDate.toISOString()}>
+                  <Badge variant="outline" className="text-sm">
+                    {dayOfWeekFormatter.format(event.startDate)}
+                  </Badge>
+                  <Badge variant="outline" className="text-sm">
+                    {hourFormatter.format(event.startDate)}
+                  </Badge>
+                </time>
+              </CardFooter>
             </article>
           </Card>
         </div>
@@ -27,6 +56,10 @@ export default function OurTeamPage() {
 
 const dayOfWeekFormatter = new Intl.DateTimeFormat("bg", {
   weekday: "long",
+});
+const hourFormatter = new Intl.DateTimeFormat("bg", {
+  hour: "numeric",
+  minute: "numeric",
 });
 const monthFormatter = new Intl.DateTimeFormat("bg", {
   month: "long",
