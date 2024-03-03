8419e923455ad9c8de0b0584dfb9cd647e84a4b8
bvpav <bvpav@tutanota.com>
2024-03-03 03:45:37 +0200

    got it validating the param

--- app/_integrations/github/app.ts
+++ app/_integrations/github/app.ts
@@ -5,6 +5,10 @@ import { SECOND } from "~/app/utils";
 
 export const app = new App({
   appId: env.GITHUB_APP_ID,
+  // NOTE: key must be in PKCS#8 format
+  // To convert:
+  //      $ openssl pkcs8 -topk8 -inform PEM -outform PEM -in leaks2.pem -out leaks2_pkcs8.pem -nocrypt
+  // (from ChatGPT, but it works, so cool ig??)
   privateKey: env.GITHUB_PRIVATE_KEY,
   webhooks: {
     secret: env.GITHUB_WEBHOOK_SECRET,
--- app/api/github/install/route.ts
+++ app/api/github/install/route.ts
@@ -1,6 +1,35 @@
 import { redirect } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
-export function GET(req: NextRequest) {
-  redirect("/github/success");
+import { getInstallationById } from "~/app/_integrations/github/installations";
+import { getParticipantFromSession } from "~/app/participants/service";
+import { signInRedirectCustom } from "../../auth/session";
+
+export async function GET(req: NextRequest) {
+  const participant = await getParticipantFromSession();
+  if (!participant) {
+    return signInRedirectCustom(req.url);
+  }
+  const setupAction = req.nextUrl.searchParams.get("setup_action");
+  if (setupAction === "request") {
+    return redirect("/github/request");
+  }
+  // NOTE: commented, so we always save the installation to cover more bases
+  // if (setupAction !== "install") {
+  //   return redirect("/github/success");
+  // }
+  const res = z
+    .string()
+    .transform((s) => parseInt(s, 10))
+    .pipe(z.number().int())
+    .safeParse(req.nextUrl.searchParams.get("installation_id"));
+  if (!res.success) {
+    return redirect("/github/error?c=wp0rgu8");
+  }
+  const installation = await getInstallationById(res.data);
+  if (!installation) {
+    return redirect("/github/error?c=d10fkg");
+  }
+  return NextResponse.json(installation);
 }
