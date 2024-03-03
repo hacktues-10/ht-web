01c312757ffbb15e64a6bf8bf2b6ca51c02ae9c5
bvpav <bvpav@tutanota.com>
2024-03-04 00:22:16 +0200

    Made it include imported repositories

--- app/_integrations/github/actions.ts
+++ app/_integrations/github/actions.ts
@@ -8,7 +8,10 @@ import {
   getProjectByTeamId,
   getTeamById,
 } from "~/app/(full-layout)/teams/service";
-import { getParticipantFromSession } from "~/app/participants/service";
+import {
+  getParticipantFromSession,
+  Participant,
+} from "~/app/participants/service";
 import { ghGetInstallationById } from "./installations";
 import {
   getInstallationRecordByParticipantId,
@@ -16,7 +19,11 @@ import {
   Installation,
 } from "./installations/storage";
 import { ghGetRepoById, ghGetReposForInstallation, Repo } from "./repos";
-import { getRepoByGithubId, importRepo } from "./repos/storage";
+import {
+  getRepoByGithubId,
+  getReposForProject,
+  importRepo,
+} from "./repos/storage";
 
 export const getGithubRepos = async () => {
   const participant = await getParticipantFromSession();
@@ -24,7 +31,18 @@ export const getGithubRepos = async () => {
     return [];
   }
 
-  const githubRepos = await fetchGithubRepos(participant.id);
+  const [githubRepos, participantRepos] = await Promise.all([
+    fetchGithubRepos(participant.id),
+    getImportedReposForParticipant(participant),
+  ]);
+  const forbiddenRepos = new Set(
+    participantRepos
+      .filter((repo) => repo.participantId !== participant.id)
+      .map((repo) => repo.githubId),
+  );
+  const reposIdMap = new Map(
+    participantRepos.map((repo) => [repo.githubId, repo.id] as const),
+  );
   return githubRepos
     .filter(
       (
@@ -36,7 +54,7 @@ export const getGithubRepos = async () => {
     )
     .flatMap((result) =>
       result.value.repos.map((repo) => ({
-        id: null,
+        id: reposIdMap.get(repo.id) ?? null,
         githubId: repo.id,
         name: repo.full_name,
         isPrivate: repo.private,
@@ -47,6 +65,7 @@ export const getGithubRepos = async () => {
         installationId: result.value.installation.id,
       })),
     )
+    .filter((repo) => !forbiddenRepos.has(repo.githubId))
     .sort((a, b) => b.updatedAt.getTime() - a.updatedAt.getTime());
 };
 
@@ -128,4 +147,15 @@ async function fetchGithubRepos(participantId: number) {
       ),
   );
   return results;
+}
+
+async function getImportedReposForParticipant(participant: Participant) {
+  if (!participant.team.id) {
+    return [];
+  }
+  const project = await getProjectByTeamId(participant.team.id);
+  if (!project) {
+    return [];
+  }
+  return getReposForProject(project.id);
 }
