1b19729b18d9ce66ca3eef27bad7f0fbff80dac9
bvpav <bvpav@tutanota.com>
2024-03-03 02:29:30 +0200

    ...пълен член

--- app/(clean-layout)/github/request/page.tsx
+++ app/(clean-layout)/github/request/page.tsx
@@ -8,7 +8,7 @@ export default function GitHubRequestSentPage() {
       <h1 className="text-3xl font-extrabold">
         Заявката за свързване с GitHub беше изпратена!
       </h1>
-      Трябва собственика на организацията да одобри заявката.
+      Трябва собственикът на организацията да одобри заявката.
       <CloseButton />
     </>
   );
