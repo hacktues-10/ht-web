415edb878292eed4ac3894f4c3e6e0de4c731eeb
bvpav <bvpav@tutanota.com>
2024-03-03 09:08:17 +0200

    fixed nextjs build metadata errors

--- app/(clean-layout)/(github)/github/request/page.tsx
+++ app/(clean-layout)/(github)/github/request/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { CloseButton } from "../_components/close-button";
 
 export const metadata = {
--- app/(clean-layout)/(github)/github/success/page.tsx
+++ app/(clean-layout)/(github)/github/success/page.tsx
@@ -6,11 +6,6 @@ import { closePopup } from "~/app/popups";
 import { SECOND } from "~/app/utils";
 import { useWindow } from "../../use-window";
 
-export const metadata = {
-  title: "Успешно свързахте Вашия GitHub!",
-  description: "Успешно свързахте Вашия GitHub!",
-};
-
 export default function GitHubSuccessPage() {
   const window = useWindow();
   const willClose = !!window?.opener;
