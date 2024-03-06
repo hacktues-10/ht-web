518c93d18a074cb59b43e965a040ea296e48e38a
bvpav <bvpav@tutanota.com>
2024-03-07 01:04:03 +0200

    fixed ...

--- app/_integrations/github/hooks.tsx
+++ app/_integrations/github/hooks.tsx
@@ -138,11 +138,11 @@ export function useRemoveRepo(options: { repoId: number }) {
   return mutation;
 }
 
-export function useGithubRepos(props: {enabled?: boolean}) {
+export function useGithubRepos(props?: { enabled?: boolean }) {
   return useQuery({
     queryKey: REPOS_QUERY_KEY,
     queryFn: () => getGithubRepos(),
-    ...props
+    ...props,
   });
 }
 
