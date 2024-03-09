af33b6020141d53535f182e7a8a52d62f2188294
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-03-09 13:45:54 +0200

    fixed width of the divs

--- app/(full-layout)/rankings/components.tsx
+++ app/(full-layout)/rankings/components.tsx
@@ -24,53 +24,55 @@ export function Ranking({ teams, isSemifinal }: RankingProps) {
           <>Финал - ученици</>
         )}
       </h1>
-      {teams.map((team, index) => (
-        <div
-          className={cn(
-            "m-1 my-2 w-full rounded-xl p-2 text-xl  font-medium hover:bg-slate-600 sm:m-2 md:m-4",
-            team.isFinalist && isSemifinal && "bg-green-200 text-black",
-            index == 0 && !isSemifinal && "bg-yellow-200 text-black",
-            index == 1 && !isSemifinal && "bg-gray-200 text-black",
-            index == 2 && !isSemifinal && "bg-orange-800",
-            index > 2 && !isSemifinal && "border border-white",
-            !team.isFinalist && isSemifinal && "border border-white",
-          )}
-          key={index}
-        >
-          <Link
-            href={`/teams/${team.id}`}
-            className="flex w-full justify-between "
+      <div className="w-full">
+        {teams.map((team, index) => (
+          <div
+            className={cn(
+              "m-1 my-2 rounded-xl p-2 text-xl  font-medium hover:bg-slate-600 sm:m-2 md:m-4",
+              team.isFinalist && isSemifinal && "bg-green-200 text-black",
+              index == 0 && !isSemifinal && "bg-yellow-200 text-black",
+              index == 1 && !isSemifinal && "bg-gray-200 text-black",
+              index == 2 && !isSemifinal && "bg-orange-800",
+              index > 2 && !isSemifinal && "border border-white",
+              !team.isFinalist && isSemifinal && "border border-white",
+            )}
+            key={index}
           >
-            <div className="mx-2 flex w-full gap-5 ">
-              <h2 className={!isSemifinal && index < 3 ? "hidden" : "block"}>
-                {index + 1}
-              </h2>
-              {index == 0 && !isSemifinal && (
-                <GiPodiumWinner className="text-3xl" />
-              )}
-              {index == 1 && !isSemifinal && (
-                <GiPodiumSecond className="text-3xl" />
-              )}
-              {index == 2 && !isSemifinal && (
-                <GiPodiumThird className="text-3xl" />
-              )}
-              <h1
-                className={cn(
-                  "text-lg sm:text-xl",
-                  index < 3 && "font-semibold",
+            <Link
+              href={`/teams/${team.id}`}
+              className="flex w-full justify-between "
+            >
+              <div className="mx-2 flex w-full gap-5 ">
+                <h2 className={!isSemifinal && index < 3 ? "hidden" : "block"}>
+                  {index + 1}
+                </h2>
+                {index == 0 && !isSemifinal && (
+                  <GiPodiumWinner className="text-3xl" />
                 )}
-              >
-                {team.name.length > 12 && !team.name.includes(" ")
-                  ? team.name.slice(0, 12) + " " + team.name.slice(12)
-                  : team.name}
-              </h1>
-            </div>
-            <h2 className="w-min text-end">
-              {isSemifinal ? team.semiFinalResult : team.finalResult}
-            </h2>
-          </Link>
-        </div>
-      ))}
+                {index == 1 && !isSemifinal && (
+                  <GiPodiumSecond className="text-3xl" />
+                )}
+                {index == 2 && !isSemifinal && (
+                  <GiPodiumThird className="text-3xl" />
+                )}
+                <h1
+                  className={cn(
+                    "text-lg sm:text-xl",
+                    index < 3 && "font-semibold",
+                  )}
+                >
+                  {team.name.length > 12 && !team.name.includes(" ")
+                    ? team.name.slice(0, 12) + " " + team.name.slice(12)
+                    : team.name}
+                </h1>
+              </div>
+              <h2 className="w-min text-end">
+                {isSemifinal ? team.semiFinalResult : team.finalResult}
+              </h2>
+            </Link>
+          </div>
+        ))}
+      </div>
     </Card>
   );
 }
