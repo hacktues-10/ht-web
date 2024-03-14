53c3c932e85e8daa0f3b0ed2697c4615cca3cd53
bvpav <78971592+bvpav@users.noreply.github.com>
2024-03-14 22:08:55 +0200

    added message sending endpoint

---
+++ app/api/teams/[id]/message/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { getTeamById } from "~/app/(full-layout)/teams/service";
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  if (params.id.includes(",")) {
+    return batchSendMessages(params.id.split(","), req);
+  }
+  return sendSingleMessage(params.id);
+}
+
+async function batchSendMessages(teamIds: string[], req: NextRequest) {
+  const teams = await Promise.all(teamIds.map(getTeamById));
+  // send the same request to all team endpoints using fetch, but changing the url:
+  const messagePromises = teams.map(async (team) => {
+    if (!team) {
+      return;
+    }
+    return fetch(
+      `https://discord.com/api/10/channels/${team.discordTextChannelId}/messages`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(await req.clone().json()),
+      },
+    );
+  });
+
+  return await Promise.all(messagePromises);
+}
+
+async function sendSingleMessage(teamId: string) {
+  const team = await getTeamById(teamId);
+  if (!team) {
+    return;
+  }
+  return NextResponse.redirect(
+    `https://discord.com/api/10/channels/${team.discordTextChannelId}/messages`,
+    {
+      status: 307,
+    },
+  );
+}
