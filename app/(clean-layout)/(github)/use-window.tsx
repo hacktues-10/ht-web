1effe31f745b0f417d826c2b439ee8c40f2dee7a
Dimitar Zhelev <dimitar.n.zhelev.2020@elsys-bg.org>
2024-06-29 13:57:45 +0300

    deleted /api + (clean-layout) + middleware

--- app/(clean-layout)/(github)/use-window.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export function useWindow() {
-  const [currentWindow, setCurrentWindow] = useState<Window | null>(null);
-  useEffect(() => {
-    setCurrentWindow(window);
-  }, []);
-  return currentWindow;
-}
