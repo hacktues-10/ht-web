64f08c39c0048d2e1667c7f7032b27f8a6c22abf
bvpav <78971592+bvpav@users.noreply.github.com>
2024-11-23 16:42:30 +0200

    remove gradients from schedule

--- app/(full-layout)/schedule/page.tsx
+++ app/(full-layout)/schedule/page.tsx
@@ -50,23 +50,6 @@ export default function SchedulePage() {
       <div className="pb-2" />
 
       <div className="flex w-full max-w-2xl flex-col items-center justify-center gap-10 overflow-y-clip sm:gap-8">
-        {/* Gradients */}
-        <div
-          className="absolute left-1/2 top-52 -z-30 h-[30%] w-full -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(ellipse_150%_50%_at_50%_50%,#ce0e3e1c,transparent)]"
-          aria-hidden
-        />
-        <div
-          className="absolute left-0 top-[200px] -z-30 h-[1540px] w-full bg-[radial-gradient(ellipse_347%_50%_at_20px_50%,hsl(var(--brand)/5%),transparent)]"
-          aria-hidden
-        />
-        <div
-          className="absolute right-0 top-[380px] -z-30 h-[1500px] w-full bg-[radial-gradient(ellipse_320%_50%_at_50%_50%,hsl(var(--sand)/7%),transparent)]"
-          aria-hidden
-        />
-        <div
-          className="absolute left-0 top-[1300px] -z-30 h-[1040px] w-full bg-[radial-gradient(ellipse_282%_50%_at_50%_50%,hsl(var(--brand)/5%),transparent)]"
-          aria-hidden
-        />
         <WorkshopsDisplay>
           <ScheduleItemList
             events={SCHEDULE.filter((item) => item.type === "workshop")}
