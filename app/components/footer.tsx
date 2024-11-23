f93cda5cc45c4513b49ff14488298d11b98ec713
bvpav <78971592+bvpav@users.noreply.github.com>
2024-11-23 17:04:06 +0200

    removed sleepy sully

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { SOCIAL_MEDIA, SocialMedia } from "~/app/_configs/pr";
 import { IfHTFeatureOn } from "../_integrations/components";
 import { HTCurrentEventLogo } from "./logos";
-import { SleepySully } from "./logos/sleepy-sully";
 import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import {
@@ -15,7 +14,6 @@ import {
 
 export const Footer = () => (
   <footer className="relative flex flex-col items-center justify-between gap-4 bg-background px-6 pb-2 pt-6 md:flex-row md:pt-2">
-    <SleepySully className="0 -sm:translate-x-10 -sm:translate-x-10 absolute left-1/2 right-1/2 top-0 h-32 -translate-x-1/2 -translate-y-full overflow-visible sm:left-auto sm:right-10 sm:translate-x-0" />
     <Link href="/" className="text-center text-2xl md:text-xl">
       <HTCurrentEventLogo />
     </Link>
